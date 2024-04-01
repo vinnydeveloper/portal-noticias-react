9c73dd5f98d0834a8d27c4fddfb1cf33bb5eadd4
Vinicius Oliveira <vinicius.developer@outlook.com>
2024-04-01 20:30:05 -0300

    fetch

--- src/services/newsApi/noticias.ts
+++ src/services/newsApi/noticias.ts
@@ -2,9 +2,21 @@ import { newsApi } from "./config";
 
 export const getTopHeadlines = async () => {
   try {
-    const response = await newsApi.get("/top-headlines?country=br");
+    const response = await fetch(
+      "https://newsapi.org/v2/top-headlines?country=br",
+      {
+        method: "GET",
+        headers: {
+          "x-api-key": "a7c47d67d09b410298a02f830d42c34a",
+        },
+      }
+    );
 
-    return response.data;
+    const body = await response.json();
+
+    console.log(body);
+
+    return body; // json
   } catch (error) {
     return {
       message: "Erro ao tentar consumir api",
